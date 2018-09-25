026587dc29b3c88973a9f4d63b5c71ea980a220a
Ryan Andersen <ryan.andersen05@gmail.com>
2018-09-25 00:03:09 -0400

    Added movement mechanics to game

--- Assets/Scripts/GameMechanics/MovementMechanics.cs
+++ Assets/Scripts/GameMechanics/MovementMechanics.cs
@@ -2,42 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CustomPhysics2D))]
 public class MovementMechanics : MonoBehaviour {
+    private const float Run_Threshold = .6f;
+    private const float Walk_Threshold = .1f;
 
     #region main variables
-
+    [Header("Mono References")]
+    [Tooltip("A reference to the sprite renderer object")]
+    public SpriteRenderer spriteRenderer;
     [Header("Ground Movement")]
     [Tooltip("The maximum walking speed")]
     public float walkingSpeed = 2f;
     [Tooltip("The maximum running speed")]
     public float runningSpeed = 5f;
     [Tooltip("The units per second that our speed will increase")]
-    public float acceleration = 25f;
+    public float groundAcceleration = 25f;
 
     /// <summary>
-    /// The current speed or our character
+    /// The custom physics component reference
     /// </summary>
-    private float currentSpeed;
+    private CustomPhysics2D rigid;
 
+    /// <summary>
+    /// The last horizontal input that was passed in
+    /// </summary>
+    private float horizontalInput;
     #endregion main variables
 
     #region monobehaivour methods
-    private void Start()
+    private void Awake()
     {
-        
+        rigid = GetComponent<CustomPhysics2D>();
     }
 
     private void Update()
     {
-        
+        UpdateCurrentSpeedOnGround();
     }
     #endregion monobehaviour methods
+    /// <summary>
+    /// Sets the horizontal input that will determine the speed that our character will move
+    /// </summary>
+    /// <param name="horizontalInput"></param>
+    public void SetHorizontalInput(float horizontalInput)
+    {
+        this.horizontalInput = horizontalInput;
+        FlipSpriteBasedOnInput(this.horizontalInput);
+    }
 
     /// <summary>
     /// Updates the speed of our character while they are grounded
     /// </summary>
     private void UpdateCurrentSpeedOnGround()
     {
+        float goalSpeed = 0;
+        if (Mathf.Abs(horizontalInput) > Run_Threshold)
+        {
+            goalSpeed = runningSpeed * Mathf.Sign(horizontalInput);
+        }
+        else if (Mathf.Abs(horizontalInput) > Walk_Threshold)
+        {
+            goalSpeed = walkingSpeed * Mathf.Sign(horizontalInput);
+        }
+        Vector2 newVelocityVector = new Vector2(rigid.velocity.x, rigid.velocity.y);
+        newVelocityVector.x = Mathf.MoveTowards(rigid.velocity.x, goalSpeed, Time.deltaTime * groundAcceleration);
+    }
 
+    /// <summary>
+    /// Flips the character's sprite appropriately based on the input passed through
+    /// </summary>
+    /// <param name="horizontalInput"></param>
+    private void FlipSpriteBasedOnInput(float horizontalInput)
+    {
+        
     }
 }
