00fdf3557c9ee9d4f46b2970af5b014a36980d74
Ryan Andersen <ryan.andersen05@gmail.com>
2018-09-25 00:24:39 -0400

    Adding a controller script

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+    public MovementMechanics movementMechanics;
+
+    private void Awake()
+    {
+        movementMechanics = GetComponent<MovementMechanics>();
+    }
+}
---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class PlayerController : MonoBehaviour {
+    public const string HorizontalInput = "Horizontal";
+
+    [Tooltip("Reference to the player stats component. This will hold all reference of our character")]
+    private CharacterStats characterStats;
+
+    #region monobehaviour methods
+    private void Start()
+    {
+        characterStats = GetComponent<CharacterStats>();
+    }
+
+    private void Update()
+    {
+        characterStats.movementMechanics.SetHorizontalInput(Input.GetAxisRaw(HorizontalInput));
+    }
+    #endregion monobehaiovur methods
+}
