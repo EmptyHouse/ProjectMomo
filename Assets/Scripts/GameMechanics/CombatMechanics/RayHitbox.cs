86df56bb5de2b1c2c4765a6926fa06c9ec1a64f1
Ryan Andersen <ryan.andersen05@gmail.com>
2018-10-07 13:46:41 -0400

    Setting so that hitboxes only interact with hitboxes

--- Assets/Scripts/GameMechanics/CombatMechanics/RayHitbox.cs
+++ Assets/Scripts/GameMechanics/CombatMechanics/RayHitbox.cs
@@ -8,16 +8,38 @@ using UnityEngine;
 /// that the hitboxes never interesct. For cases like this, it is best to use a ray hitbox
 /// </summary>
 public class RayHitbox : Hitbox {
-
+    #region main variables
+    [Tooltip("The number of rays that we will fire to see what we interact with")]
+    public int rayCount = 2;
+    [Tooltip("The length of our raycasts.")]
+    public float rayDistance;
+    #endregion main variables
     #region monobehaviour methods
     private void Update()
     {
         
     }
+
+    private void OnValidate()
+    {
+        if (rayCount < 1)
+        {
+            rayCount = 1;
+        }
+    }
     #endregion monobehaviour methods
 
-    public void CheckHitboxEntered()
+    /// <summary>
+    /// 
+    /// </summary>
+    public void RayCheckHitboxEntered()
     {
+        RaycastHit2D hit;
+        Vector3 position = transform.position;
+        Vector3 forwardDirection = transform.right;
+        Ray2D ray = new Ray2D(new Vector2(position.x, position.y), forwardDirection);
+
+        hit = Physics2D.Raycast(ray.origin, ray.direction, rayDistance);
 
     }
 }
