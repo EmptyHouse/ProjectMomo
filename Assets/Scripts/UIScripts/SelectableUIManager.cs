28cd41815e589b9ad514aa53cdf4fac38744e27a
Ryan Andersen <ryan.andersen05@gmail.com>
2018-11-11 18:57:28 -0500

    Adding auto scrolling for UI elements

--- Assets/Scripts/UIScripts/SelectableUIManager.cs
+++ Assets/Scripts/UIScripts/SelectableUIManager.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Class that will act as the manager script for all attached selectable UI elements
+/// This will open and close the menu as well as carry out any actions required when selectint
+/// UI elements
+/// </summary>
 public class SelectableUIManager : MonoBehaviour {
     #region const variables
     /// <summary>
     /// The minimum absolute value of our joystick axis, before it is registered
     /// as an input in our settings manager
     /// </summary>
-    protected const float JOYTICK_THRESHOLD = .65f;
+    protected const float JOYSTICK_THRESHOLD = .65f;
     #endregion const variables
     #region main variables
     [Tooltip("This is the time before we begin autoscrolling in our menu. We will always move to a new item immediately, but we may want a delay before scrolling to different optins automatically")]
@@ -37,16 +43,57 @@ public class SelectableUIManager : MonoBehaviour {
         {
             return;
         }
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Mathf.Abs(verticalInput) > JOYSTICK_THRESHOLD)
+        {
+
+        }
+        else if (Mathf.Abs(horizontalInput) > JOYSTICK_THRESHOLD)
+        {
+
+        }
     }
     #endregion monobehaviour methods
-
-    private IEnumerator BeginAutoScrollingVertial()
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="initialVerticalInput"></param>
+    /// <returns></returns>
+    private IEnumerator BeginAutoScrollingVertial(float initialVerticalInput)
     {
-        yield break;
-    }
+        isCurrentlyAutoScrolling = true;
+        float direction = Mathf.Sign(initialVerticalInput);
 
-    private IEnumerator BeginAutoScrollingHorizontal()
-    {
+        float timeThatHasPassed = 0;
+
+        ///TO-DO Add code that will change to the next item in the ui menu
+
+        while (timeThatHasPassed < timeBeforeAutoScrolling)
+        {
+            if (direction * Input.GetAxisRaw("Vertical") < JOYSTICK_THRESHOLD)
+            {
+                this.isCurrentlyAutoScrolling = false;
+                yield break;
+            }
+            timeThatHasPassed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        timeThatHasPassed = 0;
+        while (direction * Input.GetAxisRaw("Vertical") > JOYSTICK_THRESHOLD)
+        {
+            if (timeThatHasPassed > timeToScrollToNextOption)
+            {
+                timeThatHasPassed = 0;
+                ///TO-DO Add code that will change to the next item in the ui menu
+
+            }
+
+            timeThatHasPassed += Time.unscaledDeltaTime;
+            yield return null;
+        }
         yield break;
     }
 }
