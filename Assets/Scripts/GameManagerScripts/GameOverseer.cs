62c3c0c29d419e0ef7369648ee88367251e9fd25
Ryan Andersen <ryan.andersen05@gmail.com>
2018-11-11 17:53:03 -0500

    Adding a game overseer script. may be a requirement down the line

---
+++ Assets/Scripts/GameManagerScripts/GameOverseer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverseer : MonoBehaviour {
+    #region enums
+    public enum GameState
+    {
+        GamePlaying,
+        MenuOpen,
+    }
+
+    #endregion enums
+
+    #region static variables
+    private static GameOverseer instance;
+
+    public static GameOverseer Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<GameOverseer>();
+            }
+            return instance;
+        }
+    }
+    #endregion static variables
+
+    #region main variables
+    public CharacterStats playerCharacterStats;
+    public GameState currentGameState { get; private set; }
+    #endregion main variables
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion monobehaviour methods
+    /// <summary>
+    /// Sets the current game state of our game. Certain actions may need to be taken for certain states
+    /// </summary>
+    public void SetCurrentGameState(GameState gameStateToSet)
+    {
+        if (gameStateToSet == this.currentGameState)
+        {
+            Debug.LogWarning("You are set the state " + gameStateToSet.ToString() + " when it is already the current state");
+            return;
+        }
+        this.currentGameState = gameStateToSet;
+    }
+
+}
