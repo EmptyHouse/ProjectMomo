cb4633f34bb7db3843405fb51dfbd1a71627244c
Ryan Andersen <ryan.andersen05@gmail.com>
2018-11-24 00:42:26 -0500

    Adding some basic state machine functionality for AI

---
+++ Assets/Scripts/AIScripts/BaseEnemyStatMachine.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The base class that will handle enemy AI. This should not be used for NPC's that are 
+/// </summary>
+public class BaseEnemyStatMachine : MonoBehaviour {
+    public CharacterStats associatedCharacterStats;
+    public NPCState initialNPCState;
+
+    private NPCState currentNPCState;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void StartNPCStateMachine()
+    {
+
+    }
+
+    /// <summary>
+    /// This method will be called every frame
+    /// </summary>
+    private void UpdateNPCStateMachine()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void EndNPCStateMachine()
+    {
+
+    }
+
+
+
+    public class NPCState
+    {
+
+
+        #region state methods
+        /// <summary>
+        /// Any initialization that needs to occur when we start a new state should
+        /// take place here
+        /// </summary>
+        public void StartState()
+        {
+
+        }
+
+        /// <summary>
+        /// Anything that needs to be updated every frame when our character is in this state
+        /// should take place here.
+        /// </summary>
+        public void UpdateState()
+        {
+
+        }
+
+        /// <summary>
+        /// Any cleanup that should occur in this state should take place here.
+        /// </summary>
+        public void EndState()
+        {
+
+        }
+        #endregion state methods
+    }
+}
