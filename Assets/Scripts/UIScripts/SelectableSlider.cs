7961d071282efdc350a5e61ecb7cd4d01f242bef
Ryan Andersen <ryan.andersen05@gmail.com>
2018-11-29 02:02:19 -0500

    Adding a slider selectable UI script for menus

---
+++ Assets/Scripts/UIScripts/SelectableSlider.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SelectableSlider : SelectableUI {
+    #region main variables
+    public Slider sliderUIReference;
+    [Tooltip("Set this value to true to round all values to integer values")]
+    public bool useIntValue = false;
+    [Tooltip("The minimum value that our slider element can be")]
+    public float minSliderValue = 0;
+    [Tooltip("The maximum value that our slider element can be")]
+    public float maxSliderValue = 1;
+    [Tooltip("The value that will ")]
+    public float incrementValue = .01f;
+
+    private float currentlyDisplayedValue = 0;
+    #endregion main variables
+    #region monobehaviour methods
+    private void Update()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+        if (incrementValue < 0)
+        {
+            incrementValue = 0;
+        }
+
+        if (useIntValue)
+        {
+            if (incrementValue < 1)
+            {
+                incrementValue = 1;
+            }
+            incrementValue = (int)incrementValue;
+        }
+    }
+    #endregion monobehaviour methods
+
+    /// <summary>
+    /// Set the slider value
+    /// </summary>
+    /// <param name="valueToSetSlider"></param>
+    public void SetValue(float valueToSetSlider)
+    {
+        if (valueToSetSlider < minSliderValue)
+        {
+            sliderUIReference.value = 0;
+        }
+        else if (valueToSetSlider > maxSliderValue)
+        {
+            sliderUIReference.value = 1;
+        }
+    }
+
+}
