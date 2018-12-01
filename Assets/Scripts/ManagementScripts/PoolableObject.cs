aaa479a485ac91980831da946807975668d0350d
Ryan Andersen <ryan.andersen05@gmail.com>
2018-12-01 14:59:58 -0500

    Adding some code to help with spawn pooling objects in the future
    -Optimization stuff not too important right now

---
+++ Assets/Scripts/ManagementScripts/PoolableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface PoolableObject {
+    /// <summary>
+    /// Any setup and preparation that should be taken care of when a poolable object is spawned should take place here
+    /// </summary>
+    void OnObjectSpawned();
+
+    /// <summary>
+    /// Any clean up that should occur when our object is despawned should occur here. Essentially can act as the destroy method, but
+    /// be sure to keep in mind that this object may be reused so be careful when running clean up
+    /// </summary>
+    void OnObjectDespawned();
+
+    /// <summary>
+    /// Spawnable objects must be monobehaviour objects. We will need to have access to the attached gameobject for poolable objects
+    /// to function correctly
+    /// </summary>
+    MonoBehaviour attachedMonobehaviour { get; }
+}
---
+++ Assets/Scripts/ManagementScripts/SpawnPool.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPool : MonoBehaviour {
+    #region static varaibles
+
+    public static SpawnPool Instance;
+    #endregion static variables
+
+
+
+    private Dictionary<string, List<PoolableObject>> spawnPoolDictionary = new Dictionary<string, List<PoolableObject>>();
+
+
+    
+}
