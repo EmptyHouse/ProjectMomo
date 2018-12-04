6eed89fa34dfe9a9f956381d30c65b9b1e5d4046
Ryan Andersen <ryan.andersen05@gmail.com>
2018-12-04 01:10:55 -0500

    Adding basic enemy to hit. (not ready)
    Also adding a Game Debugger script that should come in handy down the road

---
+++ Assets/Scripts/GameDebugger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// In this method we will have access to a bunch of debuggin methods that will help us along with development to
+/// ensure that everything is being implemented correctly
+/// </summary>
+public class GameDebugger : MonoBehaviour {
+    #region static variables
+    private static GameDebugger instance;
+    public static GameDebugger Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<GameDebugger>();
+            }
+            return instance;
+        }
+    }
+    #endregion static variables
+
+
+    public bool displayCharacterHealthbars = false;
+	
+}
--- Assets/Scripts/GameMechanics/CombatMechanics/Hurtbox.cs
+++ Assets/Scripts/GameMechanics/CombatMechanics/Hurtbox.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class will contain all the logic necessary for our character's hitbox
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
 public class Hurtbox : MonoBehaviour {
-    [System.NonSerialized]
-    public MeleeMechanics associatedMeleeMechanics;
+    public MeleeMechanics associatedMeleeMechanics { get; set; }
     #region monobehaviour methods
     private void Awake()
     {
