43edb633d6c48a7dc6fea0c5b313965ab72f1db9
Ryan Andersen <ryan.andersen05@gmail.com>
2019-03-24 17:09:47 -0400

    Adding damage taken text script

---
+++ Assets/Scripts/UIScripts/CharacterDamageText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDamageText : MonoBehaviour
+{ 
+    public enum DamageTextType
+    {
+        HealingText,
+        DamageTakenText,
+    }
+
+    private DamageTextType damageTextType;
+    public void SetupDamageText(DamageTextType typeOfText, float valueToDisplay)
+    {
+
+    }
+}
